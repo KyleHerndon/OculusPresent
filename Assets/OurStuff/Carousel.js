93c4379dcc0b383bc3addd805c008775b1649da5
Zach Wade <zacharywade@gmail.com>
2014-09-13 09:36:24 -0400

    pushing kerning

--- Assets/OurStuff/Carousel.js
+++ Assets/OurStuff/Carousel.js
@@ -1,8 +1,10 @@
 ﻿#pragma strict
 
+import TextSize;
+
 var slides = new Array();
 
-var CONSTELLATION : float = 45;
+var CONSTELLATION : float = 70;
 var RADIUS : float = 5;
 var ROTATION_SPEED :float = 75;
 var MAX_SLIDES : int = 4;
@@ -10,8 +12,12 @@ var MAX_SLIDES : int = 4;
 var rotateDist : float = -1;
 var prefab : GameObject;
 
-function Start () {
+var titleFab : GameObject;
+var bodyFab  : GameObject;
 
+private var ts:TextSize;
+
+function Start () {
 }
 
 function Update () {
@@ -25,23 +31,67 @@ function Update () {
 }
 
 
-function AddToCarousel(slide : Texture) {
+function AddToCarousel(slide : Texture, params:JSONNode) {
 	if (rotateDist > 0) {
-		Debug.Log("false");
 		return false;
 	}
 	var newSlide : GameObject = Instantiate( prefab );
 	newSlide.transform.position = Vector3(-RADIUS,0,0);
-	Debug.Log(newSlide.transform.position);
 	newSlide.transform.rotation.eulerAngles = Vector3(0, 90, 0);
 	newSlide.renderer.materials[0].mainTexture = slide;
+	
+	var tm:TextMesh;
+	
+	if (params["title"]) {
+		var newTitle : GameObject = Instantiate( titleFab );
+		tm = (newTitle.GetComponent(TextMesh) as TextMesh);
+		tm.text = params["title"];
+		var bounds:Bounds = tm.renderer.bounds;
+		newTitle.transform.localScale.x = 1.0/bounds.size.z * newSlide.transform.localScale.x - 0.1;
+		newTitle.transform.parent = newSlide.transform;
+		newTitle.transform.localPosition = Vector3( 0 , 0.4, 0.5);
+	}
+	
+	if (params["text"]) {
+		
+		var newBody : GameObject = Instantiate( bodyFab );
+		tm = (newBody.GetComponent(TextMesh) as TextMesh);
+		newBody.transform.localScale.x = newSlide.transform.localScale.z*.1;
+		newBody.transform.localScale.y = newSlide.transform.localScale.y*.1;
+		newBody.transform.parent = newSlide.transform;
+		newBody.transform.localPosition = Vector3( 0.49, 0.25, 0.5);
+		ts = new TextSize(tm);
+		var words:String[] = params["text"].ToString().Split(" "[0]);
+		var fin  : String = "";
+		var oldS : String = "";
+		var newS : String = "";
+		for (var s : String in words) {
+			newS += s+" ";
+			Debug.Log(ts.GetTextWidth(newS));
+			if (ts.GetTextWidth(newS) > newSlide.transform.localScale.x) {
+				fin+=oldS+"\n";
+				newS = s+" ";
+				oldS = "";
+			} else {
+				oldS = newS;
+			}
+		}
+		fin += newS;
+		Debug.Log(fin);
+		tm.text = fin;
+		
+		Debug.Log(ts.GetTextWidth(fin));
+		newBody.transform.rotation.eulerAngles = Vector3(0 , -90, 0);
+			
+	}
 	
 	slides.Add(newSlide);
 	
 	if (slides.length > MAX_SLIDES) {
-		Debug.Log("remove");
 		RemoveFromCarousel();
 	}
+	
+	return true;
 }
 
 function RemoveFromCarousel() {
--- Assets/OurStuff/GenerateSlide.js
+++ Assets/OurStuff/GenerateSlide.js
@@ -1,19 +1,33 @@
 ﻿#pragma strict
 
+import SimpleJSON;
+
 var prefab   : GameObject;
 var tmpText  : Texture;
+var layout   : TextAsset;
+
+private var slides:JSONNode;
 
 private var carousel : Carousel;
 
+
 function Start () {
 	carousel = GetComponent(Carousel) as Carousel;
+	slides = JSONNode.Parse(layout.ToString());
+	Debug.Log(slides);
+	var url:String = "https://www.google.com/images/srpr/logo11w.png";
+	var www:WWW = new WWW(url);
+	yield www;
+	tmpText = www.texture;
 }
 
 function Update () {
 	if (Input.GetKeyDown("right")) {
 		Debug.Log("right");
-		carousel.AddToCarousel(tmpText);
-		carousel.RotateCarousel();
+		var added : boolean = carousel.AddToCarousel(tmpText, slides["slides"][0]);
+		if (added) {
+			carousel.RotateCarousel();
+		}
 		
 	}
 
