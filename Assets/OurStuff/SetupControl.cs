aec3dd9bab5cd691c73a8a0d9cf4af3f694d864e
Ellis <el1t@ymail.com>
2014-09-14 09:11:56 -0400

    better loader

--- Assets/OurStuff/SetupControl.cs
+++ Assets/OurStuff/SetupControl.cs
@@ -2,29 +2,31 @@
 using System.Collections;
 
 public class SetupControl : MonoBehaviour {
-
 	private bool showGui = true;
 	private string text = "";
 	private bool click = false;
 
 	public GameObject slideshow;
+	public GameObject plane;
+	public GUISkin skin;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
 	void Update () {
-			
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			slideshow.SendMessage("LoadFile",text);
+			plane.SetActive(true);
+			gameObject.SetActive(false);
+		}
 	}
 
 	void OnGUI() {
+		GUI.skin = skin;
 		GUI.Box (new Rect(Screen.width/2 - 250, Screen.height/2+200, 500, 100), GUIContent.none);
-		text = GUI.TextArea(new Rect(Screen.width/2 - 225, Screen.height/2+225, 300, 50), text);
+		text = GUI.TextField(new Rect(Screen.width/2 - 225, Screen.height/2+225, 300, 50), text);
 		click = GUI.Button (new Rect(Screen.width/2 + 100, Screen.height/2+225, 125, 50), "Load App");
 		if (click) {
 			slideshow.SendMessage("LoadFile",text);
-			this.transform.gameObject.SetActive(false);
+			plane.SetActive(true);
+			gameObject.SetActive(false);
 		}
 	}
 }
